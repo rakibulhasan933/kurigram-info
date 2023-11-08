08c9f342e4a424577a662ebc9f44b113a8a20190
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-08 20:42:03 +0600

    [ADD] cover photo added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,17 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h2>Kurigram Info</h2>
+      <div className="my-2">
+        <div className="flex flex-col items-center gap-2">
+          <div className="w-full relative">
+            {/* Image Size 1280X400 */}
+            <Image src="/london.jpg" className='w-full object-cover' priority={true} width={1280} height={400} alt='banner' />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+              <h1 className=" md:text-4xl text-xl  text-center md:mb-12 mb-2 text-white font-extrabold">KURIGRAM</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }

9f2088da553104ef2ad0f44794e0d4d12934b968
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-10 00:38:43 +0600

    [ADD] top hotel

--- src/components/TopHotels.tsx
+++ src/components/TopHotels.tsx
@@ -11,74 +11,82 @@ function TopHotels() {
 				<Link className=' px-4 py-2 hover:text-white w-fit hover:bg-pink-400 rounded text-sm text-pink-400 font-semibold border-2 border-pink-400' href="/">See All</Link>
 			</div>
 			<div className="grid md:grid-cols-4 grid-cols-2 gap-3">
-				<div className="flex justify-center flex-col mt-4 rounded-md border-[1px] shadow-lg">
-					<Image src="/room.jpg" className=' rounded-md object-cover w-full' alt='room' width={300} height={200} priority={true} />
-					<div className="px-2 py-4 flex flex-col gap-x-3">
-						<h2 className="font-bold text-lg  hover:text-pink-400">Pera Palace Hotel</h2>
-						<p className="text-sm font-semibold mb-2">Become a witness of the past with..</p>
-						<h2 className="flex flex-row mr-2 mb-2 hover:text-pink-400"><LocateFixedIcon className='w-4 mr-2  text-pink-400' />Kurigram</h2>
-						<h2 className="flex flex-row mr-2 hover:text-pink-400"><PhoneCallIcon className='w-4 mr-2 text-pink-400' />+8801793874052</h2>
-						<hr className='w-full my-2' />
-						<div className="flex justify-between">
-							<div className="flex flex-row gap-y-2">
-								<HotelIcon className='w-10 text-pink-400' />
-								<h2 className="text-lg font-medium">Hotels</h2>
+				<Link href="/">
+					<div className="flex justify-center flex-col mt-4 rounded-md border-[1px] hover:border-pink-400 hover:border-2 shadow-lg">
+						<Image src="/room.jpg" className=' rounded-md object-cover w-full' alt='room' width={300} height={200} priority={true} />
+						<div className="px-2 py-4 flex flex-col gap-x-3">
+							<h2 className="font-bold text-lg  hover:text-pink-400">Pera Palace Hotel</h2>
+							<p className="text-sm font-semibold mb-2">Become a witness of the past with..</p>
+							<h2 className="flex flex-row mr-2 mb-2 hover:text-pink-400"><LocateFixedIcon className='w-4 mr-2  text-pink-400' />Kurigram</h2>
+							<h2 className="flex flex-row mr-2 hover:text-pink-400"><PhoneCallIcon className='w-4 mr-2 text-pink-400' />+8801793874052</h2>
+							<hr className='w-full my-2' />
+							<div className="flex justify-between">
+								<div className="flex flex-row gap-y-2">
+									<HotelIcon className='w-10 text-pink-400' />
+									<h2 className="text-lg font-medium">Hotels</h2>
+								</div>
+								<HeartIcon className='w-10 hover:text-pink-400' />
 							</div>
-							<HeartIcon className='w-10 hover:text-pink-400' />
 						</div>
 					</div>
-				</div>
-				<div className="flex justify-center flex-col mt-4 rounded-md border-[1px] shadow-lg">
-					<Image src="/room.jpg" className=' rounded-md object-cover w-full' alt='room' width={300} height={200} priority={true} />
-					<div className="px-2 py-4 flex flex-col gap-x-3">
-						<h2 className="font-bold text-lg  hover:text-pink-400">Pera Palace Hotel</h2>
-						<p className="text-sm font-semibold mb-2">Become a witness of the past with..</p>
-						<h2 className="flex flex-row mr-2 mb-2 hover:text-pink-400"><LocateFixedIcon className='w-4 mr-2  text-pink-400' />Kurigram</h2>
-						<h2 className="flex flex-row mr-2 hover:text-pink-400"><PhoneCallIcon className='w-4 mr-2 text-pink-400' />+8801793874052</h2>
-						<hr className='w-full my-2' />
-						<div className="flex justify-between">
-							<div className="flex flex-row gap-y-2">
-								<HotelIcon className='w-10 text-pink-400' />
-								<h2 className="text-lg font-medium">Hotels</h2>
+				</Link>
+				<Link href="/">
+					<div className="flex justify-center flex-col mt-4 rounded-md border-[1px] hover:border-pink-400 hover:border-2 shadow-lg">
+						<Image src="/room.jpg" className=' rounded-md object-cover w-full' alt='room' width={300} height={200} priority={true} />
+						<div className="px-2 py-4 flex flex-col gap-x-3">
+							<h2 className="font-bold text-lg  hover:text-pink-400">Pera Palace Hotel</h2>
+							<p className="text-sm font-semibold mb-2">Become a witness of the past with..</p>
+							<h2 className="flex flex-row mr-2 mb-2 hover:text-pink-400"><LocateFixedIcon className='w-4 mr-2  text-pink-400' />Kurigram</h2>
+							<h2 className="flex flex-row mr-2 hover:text-pink-400"><PhoneCallIcon className='w-4 mr-2 text-pink-400' />+8801793874052</h2>
+							<hr className='w-full my-2' />
+							<div className="flex justify-between">
+								<div className="flex flex-row gap-y-2">
+									<HotelIcon className='w-10 text-pink-400' />
+									<h2 className="text-lg font-medium">Hotels</h2>
+								</div>
+								<HeartIcon className='w-10 hover:text-pink-400' />
 							</div>
-							<HeartIcon className='w-10 hover:text-pink-400' />
 						</div>
 					</div>
-				</div>
-				<div className="flex justify-center flex-col mt-4 rounded-md border-[1px] shadow-lg">
-					<Image src="/room.jpg" className=' rounded-md object-cover w-full' alt='room' width={300} height={200} priority={true} />
-					<div className="px-2 py-4 flex flex-col gap-x-3">
-						<h2 className="font-bold text-lg  hover:text-pink-400">Pera Palace Hotel</h2>
-						<p className="text-sm font-semibold mb-2">Become a witness of the past with..</p>
-						<h2 className="flex flex-row mr-2 mb-2 hover:text-pink-400"><LocateFixedIcon className='w-4 mr-2  text-pink-400' />Kurigram</h2>
-						<h2 className="flex flex-row mr-2 hover:text-pink-400"><PhoneCallIcon className='w-4 mr-2 text-pink-400' />+8801793874052</h2>
-						<hr className='w-full my-2' />
-						<div className="flex justify-between">
-							<div className="flex flex-row gap-y-2">
-								<HotelIcon className='w-10 text-pink-400' />
-								<h2 className="text-lg font-medium">Hotels</h2>
+				</Link>
+				<Link href="/">
+					<div className="flex justify-center flex-col mt-4 rounded-md border-[1px] hover:border-pink-400 hover:border-2 shadow-lg">
+						<Image src="/room.jpg" className=' rounded-md object-cover w-full' alt='room' width={300} height={200} priority={true} />
+						<div className="px-2 py-4 flex flex-col gap-x-3">
+							<h2 className="font-bold text-lg  hover:text-pink-400">Pera Palace Hotel</h2>
+							<p className="text-sm font-semibold mb-2">Become a witness of the past with..</p>
+							<h2 className="flex flex-row mr-2 mb-2 hover:text-pink-400"><LocateFixedIcon className='w-4 mr-2  text-pink-400' />Kurigram</h2>
+							<h2 className="flex flex-row mr-2 hover:text-pink-400"><PhoneCallIcon className='w-4 mr-2 text-pink-400' />+8801793874052</h2>
+							<hr className='w-full my-2' />
+							<div className="flex justify-between">
+								<div className="flex flex-row gap-y-2">
+									<HotelIcon className='w-10 text-pink-400' />
+									<h2 className="text-lg font-medium">Hotels</h2>
+								</div>
+								<HeartIcon className='w-10 hover:text-pink-400' />
 							</div>
-							<HeartIcon className='w-10 hover:text-pink-400' />
 						</div>
 					</div>
-				</div>
-				<div className="flex justify-center flex-col mt-4 rounded-md border-[1px] shadow-lg">
-					<Image src="/room.jpg" className=' rounded-md object-cover w-full' alt='room' width={300} height={200} priority={true} />
-					<div className="px-2 py-4 flex flex-col gap-x-3">
-						<h2 className="font-bold text-lg  hover:text-pink-400">Pera Palace Hotel</h2>
-						<p className="text-sm font-semibold mb-2">Become a witness of the past with..</p>
-						<h2 className="flex flex-row mr-2 mb-2 hover:text-pink-400"><LocateFixedIcon className='w-4 mr-2  text-pink-400' />Kurigram</h2>
-						<h2 className="flex flex-row mr-2 hover:text-pink-400"><PhoneCallIcon className='w-4 mr-2 text-pink-400' />+8801793874052</h2>
-						<hr className='w-full my-2' />
-						<div className="flex justify-between">
-							<div className="flex flex-row gap-y-2">
-								<HotelIcon className='w-10 text-pink-400' />
-								<h2 className="text-lg font-medium">Hotels</h2>
+				</Link>
+				<Link href="/">
+					<div className="flex justify-center flex-col mt-4 rounded-md border-[1px] hover:border-pink-400 hover:border-2 shadow-lg">
+						<Image src="/room.jpg" className=' rounded-md object-cover w-full' alt='room' width={300} height={200} priority={true} />
+						<div className="px-2 py-4 flex flex-col gap-x-3">
+							<h2 className="font-bold text-lg  hover:text-pink-400">Pera Palace Hotel</h2>
+							<p className="text-sm font-semibold mb-2">Become a witness of the past with..</p>
+							<h2 className="flex flex-row mr-2 mb-2 hover:text-pink-400"><LocateFixedIcon className='w-4 mr-2  text-pink-400' />Kurigram</h2>
+							<h2 className="flex flex-row mr-2 hover:text-pink-400"><PhoneCallIcon className='w-4 mr-2 text-pink-400' />+8801793874052</h2>
+							<hr className='w-full my-2' />
+							<div className="flex justify-between">
+								<div className="flex flex-row gap-y-2">
+									<HotelIcon className='w-10 text-pink-400' />
+									<h2 className="text-lg font-medium">Hotels</h2>
+								</div>
+								<HeartIcon className='w-10 hover:text-pink-400' />
 							</div>
-							<HeartIcon className='w-10 hover:text-pink-400' />
 						</div>
 					</div>
-				</div>
+				</Link>
 			</div>
 		</div>
 	)

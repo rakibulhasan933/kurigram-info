1548850f01a2063235031ccbf5afdd018da38781
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-10 23:00:52 +0600

    [ADD] google maps added

--- src/app/hotels/page.tsx
+++ src/app/hotels/page.tsx
@@ -103,14 +103,11 @@ function Hotels() {
 									<h1 className=" text-sm flex flex-row items-center font-medium  hover:text-pink-400"> <MapPin className='w-6 mr-3' />Kurigram</h1>
 								</div>
 							</div>
-							<div className="bg-white px-3 py-4 rounded">
-								<h2 className="uppercase p-1 flex flex-row font-bold"><Bookmark className='w-4 mr-1' />Map</h2>
-								<hr className='w-full' />
-								<div className="px-3 py-4  flex flex-col gap-y-5">
-									<h1 className=" text-sm flex flex-row items-center font-medium hover:text-pink-400"> <MailIcon className='w-6 mr-3' /> info@perapalace.com</h1>
-									<h1 className=" text-sm flex flex-row items-center font-medium  hover:text-pink-400"> <PhoneCallIcon className='w-6 mr-3' />+8801793874052 </h1>
-									<h1 className=" text-sm flex flex-row items-center font-medium  hover:text-pink-400"> <Globe className='w-6 mr-3' />https://www.perapalace.com</h1>
-									<h1 className=" text-sm flex flex-row items-center font-medium  hover:text-pink-400"> <MapPin className='w-6 mr-3' />Kurigram</h1>
+							<div className="bg-white rounded">
+								<h2 className="p-1 flex flex-row font-bold"><Bookmark className='w-4 mr-1' />Map</h2>
+								<hr className='w-full my-3' />
+								<div className="">
+									<iframe className='w-full rounded' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d919515.5403311296!2d89.00917749514751!3d25.80827225152525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e2c8648556f8cb%3A0xfaa7d471019abb1f!2sKurigram%20District!5e0!3m2!1sen!2sbd!4v1699635212697!5m2!1sen!2sbd" width="500" height="300" style={{ border: 0 }} loading="lazy"></iframe>
 								</div>
 							</div>
 						</div>

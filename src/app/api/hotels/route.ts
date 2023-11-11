a6a19d61ce2c09c276b90f7968738bf5adb97ea1
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-12 03:09:40 +0600

    [ADD] single route

---
+++ src/app/api/hotels/route.ts
@@ -0,0 +1,16 @@
+import prisma from "@/lib/db/prisma"
+import { NextResponse } from "next/server";
+
+
+export const GET = async () => {
+	try {
+		const result = await prisma.service.findMany({
+			where: {
+				category: "hotels"
+			}
+		});
+		return NextResponse.json(result);
+	} catch (error) {
+		return NextResponse.json(error)
+	}
+};

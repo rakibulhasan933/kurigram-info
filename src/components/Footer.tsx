91937fff0cfac86898d7314e9dc3b9984aee8217
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-11 10:07:57 +0600

    [ADD] footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,10 @@ function Footer() {
 		<div className='mx-2 md:mx-4'>
 			<div className=" bg-slate-100">
 				<div className='grid md:grid-cols-4  py-4 px-2 gap-5 rounded'>
-					<div className="flex items-center">
+					<div className="">
 						<iframe className='w-full rounded' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d919515.5403311296!2d89.00917749514751!3d25.80827225152525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e2c8648556f8cb%3A0xfaa7d471019abb1f!2sKurigram%20District!5e0!3m2!1sen!2sbd!4v1699635212697!5m2!1sen!2sbd" width="500" height="300" style={{ border: 0 }} loading="lazy"></iframe>
 					</div>
-					<div className="flex flex-col items-center">
+					<div className="flex flex-col md:px-4 px-2">
 						<h1 className='text-xl mt-8 font-bold font-sans opacity-75 underline  '>Our Services</h1>
 						<ol className='space-y-2 text-sm mt-5'>
 							<li>
@@ -27,7 +27,7 @@ function Footer() {
 							</li>
 						</ol>
 					</div>
-					<div className="flex flex-col items-center">
+					<div className="flex flex-col md:px-4 px-2">
 						<h1 className='text-xl mt-8 font-bold font-sans opacity-75 underline  '>Social Media</h1>
 						<ol className='space-y-2 text-sm mt-5'>
 							<li className='flex gap-2'>
@@ -57,7 +57,7 @@ function Footer() {
 							</li>
 						</ol>
 					</div>
-					<div className="flex flex-col items-center">
+					<div className="md:px-4 px-2">
 						<h1 className='text-xl mt-8 font-bold font-sans opacity-75 underline  '>Contact Us</h1>
 						<ol className='space-y-2 text-sm mt-5'>
 							<li className='flex gap-2'>

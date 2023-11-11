4b9442be49d35752240e13021128d2d7b6a09c9e
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-11 21:31:37 +0600

    [ADD]clerk and prisma orm and database connected

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+import Footer from '@/components/Footer';
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,12 +18,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
       <body className={inter.className}>
         <Navbar />
         {children}
         <Footer />
       </body>
-    </html>
+      </html>
+    </ClerkProvider>
   )
 }

a5cd62ab31a392b575043c993cbe389e5cad0ed0
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-11 17:55:04 +0600

    [ADD] teachers added

--- src/app/education/[id]/page.tsx
+++ src/app/education/[id]/page.tsx
@@ -102,39 +102,22 @@ function EducationCard() {
 									<h1 className=" text-sm flex flex-row items-center font-medium  hover:text-pink-400"> <MapPin className='w-6 mr-3' />Kurigram</h1>
 								</div>
 							</div>
-							<div className="grid md:grid-cols-2 grid-cols-1 gap-2 ">
+							<div className="grid md:grid-cols-2 grid-cols-1 gap-4 ">
 								<div className="flex flex-col gap-x-1 rounded-md cursor-pointer bg-white hover:border-pink-400 hover:border-[2px] shadow-xl">
-									<Image src="/pizza.jpg" className=' w-full object-cover rounded' alt='pizza' width={300} height={200} />
+									<Image src="/pizza.jpg" className=' object-cover rounded' alt='pizza' width={150} height={120} />
 									<div className="p-2">
-										<h2 className=" text-lg font-bold">Pizza</h2>
-										<h2 className="text-base font-bold flex flex-row"><span className='text-orange-400 mr-4'>4.5</span>  <Star color="#e3ad16" className='w-4' /> <Star color="#e3ad16" className='w-4' /> <Star color="#e3ad16" className='w-4' /><StarHalf className='w-4' color="#e3ad16" /></h2>
+										<h2 className=" text-lg font-bold">MD Rakibul Hasan</h2>
 										<p className="text-sm font-normal">Kurigram restaurant</p>
 									</div>
 								</div>
 								<div className="flex flex-col gap-x-1 rounded-md cursor-pointer bg-white hover:border-pink-400 hover:border-[2px] shadow-xl">
-									<Image src="/pizza.jpg" className=' w-full object-cover rounded' alt='pizza' width={300} height={200} />
+									<Image src="/pizza.jpg" className=' object-cover rounded' alt='pizza' width={150} height={120} />
 									<div className="p-2">
-										<h2 className=" text-lg font-bold">Pizza</h2>
-										<h2 className="text-base font-bold flex flex-row"><span className='text-orange-400 mr-4'>4.5</span>  <Star color="#e3ad16" className='w-4' /> <Star color="#e3ad16" className='w-4' /> <Star color="#e3ad16" className='w-4' /><StarHalf className='w-4' color="#e3ad16" /></h2>
+										<h2 className=" text-lg font-bold">MD Rakibul Hasan</h2>
 										<p className="text-sm font-normal">Kurigram restaurant</p>
 									</div>
 								</div>
-								<div className="flex flex-col gap-x-1 rounded-md cursor-pointer bg-white hover:border-pink-400 hover:border-[2px] shadow-xl">
-									<Image src="/pizza.jpg" className=' w-full object-cover rounded' alt='pizza' width={300} height={200} />
-									<div className="p-2">
-										<h2 className=" text-lg font-bold">Pizza</h2>
-										<h2 className="text-base font-bold flex flex-row"><span className='text-orange-400 mr-4'>4.5</span>  <Star color="#e3ad16" className='w-4' /> <Star color="#e3ad16" className='w-4' /> <Star color="#e3ad16" className='w-4' /><StarHalf className='w-4' color="#e3ad16" /></h2>
-										<p className="text-sm font-normal">Kurigram restaurant</p>
-									</div>
-								</div>
-								<div className="flex flex-col gap-x-1 rounded-md cursor-pointer bg-white hover:border-pink-400 hover:border-[2px] shadow-xl">
-									<Image src="/pizza.jpg" className=' w-full object-cover rounded' alt='pizza' width={300} height={200} />
-									<div className="p-2">
-										<h2 className=" text-lg font-bold">Pizza</h2>
-										<h2 className="text-base font-bold flex flex-row"><span className='text-orange-400 mr-4'>4.5</span>  <Star color="#e3ad16" className='w-4' /> <Star color="#e3ad16" className='w-4' /> <Star color="#e3ad16" className='w-4' /><StarHalf className='w-4' color="#e3ad16" /></h2>
-										<p className="text-sm font-normal">Kurigram restaurant</p>
-									</div>
-								</div>
+
 							</div>
 							<div className="bg-white rounded">
 								<h2 className="p-1 flex flex-row font-bold"><Bookmark className='w-4 mr-1' />Map</h2>

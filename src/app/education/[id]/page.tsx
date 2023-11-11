6691a773288763b9003953725ec7ec955b549776
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-11 16:09:33 +0600

    [ADD] slider set

--- src/app/education/[id]/page.tsx
+++ src/app/education/[id]/page.tsx
@@ -5,7 +5,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 // import required modules
--- src/components/TopEducations.tsx
+++ src/components/TopEducations.tsx
@@ -1,7 +1,13 @@
+"use client";
 import { GraduationCapIcon, HeartIcon, HotelIcon, LocateFixedIcon, PhoneCallIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { isMobile } from 'react-device-detect';
 
 function TopEducations() {
 	return (
@@ -10,83 +16,34 @@ function TopEducations() {
 				<h2 className="px-4 py-2 rounded md:text-xl text-sm font-medium text-white bg-pink-400 w-fit">Educational Institutions</h2>
 				<Link className=' px-4 py-2 hover:text-white w-fit hover:bg-pink-400 rounded text-sm text-pink-400 font-semibold border-2 border-pink-400' href="/education">More</Link>
 			</div>
-			<div className="grid md:grid-cols-4 grid-cols-1 gap-3">
-				<Link href="/">
-					<div className="flex justify-center flex-col mt-4 rounded-md border-[1px] hover:border-pink-400 hover:border-2 shadow-lg">
-						<Image src="/education.jpg" className=' rounded-md object-cover w-full' alt='room' width={300} height={200} priority={true} />
-						<div className="px-2 py-4 flex flex-col gap-x-3">
-							<h2 className="font-bold text-lg  hover:text-pink-400">Kurigram Govt Collage</h2>
-							<p className="text-sm font-semibold mb-2">Become a witness of the past with..</p>
-							<h2 className="flex flex-row mr-2 mb-2 hover:text-pink-400"><LocateFixedIcon className='w-4 mr-2  text-pink-400' />Kurigram</h2>
-							<h2 className="flex flex-row mr-2 hover:text-pink-400"><PhoneCallIcon className='w-4 mr-2 text-pink-400' />+8801793874052</h2>
-							<hr className='w-full my-2' />
-							<div className="flex justify-between">
-								<div className="flex flex-row gap-y-2">
-									<GraduationCapIcon className='w-10 text-pink-400' />
-									<h2 className="text-lg font-medium">Collage</h2>
-								</div>
-								<HeartIcon className='w-10 hover:text-pink-400' />
-							</div>
-						</div>
-					</div>
-				</Link>
-				<Link href="/">
-					<div className="flex justify-center flex-col mt-4 rounded-md border-[1px] hover:border-pink-400 hover:border-2 shadow-lg">
-						<Image src="/education.jpg" className=' rounded-md object-cover w-full' alt='room' width={300} height={200} priority={true} />
-						<div className="px-2 py-4 flex flex-col gap-x-3">
-							<h2 className="font-bold text-lg  hover:text-pink-400">Kurigram Govt Collage</h2>
-							<p className="text-sm font-semibold mb-2">Become a witness of the past with..</p>
-							<h2 className="flex flex-row mr-2 mb-2 hover:text-pink-400"><LocateFixedIcon className='w-4 mr-2  text-pink-400' />Kurigram</h2>
-							<h2 className="flex flex-row mr-2 hover:text-pink-400"><PhoneCallIcon className='w-4 mr-2 text-pink-400' />+8801793874052</h2>
-							<hr className='w-full my-2' />
-							<div className="flex justify-between">
-								<div className="flex flex-row gap-y-2">
-									<GraduationCapIcon className='w-10 text-pink-400' />
-									<h2 className="text-lg font-medium">Collage</h2>
-								</div>
-								<HeartIcon className='w-10 hover:text-pink-400' />
-							</div>
-						</div>
-					</div>
-				</Link>
-				<Link href="/">
-					<div className="flex justify-center flex-col mt-4 rounded-md border-[1px] hover:border-pink-400 hover:border-2 shadow-lg">
-						<Image src="/education.jpg" className=' rounded-md object-cover w-full' alt='room' width={300} height={200} priority={true} />
-						<div className="px-2 py-4 flex flex-col gap-x-3">
-							<h2 className="font-bold text-lg  hover:text-pink-400">Kurigram Govt Collage</h2>
-							<p className="text-sm font-semibold mb-2">Become a witness of the past with..</p>
-							<h2 className="flex flex-row mr-2 mb-2 hover:text-pink-400"><LocateFixedIcon className='w-4 mr-2  text-pink-400' />Kurigram</h2>
-							<h2 className="flex flex-row mr-2 hover:text-pink-400"><PhoneCallIcon className='w-4 mr-2 text-pink-400' />+8801793874052</h2>
-							<hr className='w-full my-2' />
-							<div className="flex justify-between">
-								<div className="flex flex-row gap-y-2">
-									<GraduationCapIcon className='w-10 text-pink-400' />
-									<h2 className="text-lg font-medium">Collage</h2>
-								</div>
-								<HeartIcon className='w-10 hover:text-pink-400' />
-							</div>
-						</div>
-					</div>
-				</Link>
-				<Link href="/">
-					<div className="flex justify-center flex-col mt-4 rounded-md border-[1px] hover:border-pink-400 hover:border-2 shadow-lg">
-						<Image src="/education.jpg" className=' rounded-md object-cover w-full' alt='room' width={300} height={200} priority={true} />
-						<div className="px-2 py-4 flex flex-col gap-x-3">
-							<h2 className="font-bold text-lg  hover:text-pink-400">Kurigram Govt Collage</h2>
-							<p className="text-sm font-semibold mb-2">Become a witness of the past with..</p>
-							<h2 className="flex flex-row mr-2 mb-2 hover:text-pink-400"><LocateFixedIcon className='w-4 mr-2  text-pink-400' />Kurigram</h2>
-							<h2 className="flex flex-row mr-2 hover:text-pink-400"><PhoneCallIcon className='w-4 mr-2 text-pink-400' />+8801793874052</h2>
-							<hr className='w-full my-2' />
-							<div className="flex justify-between">
-								<div className="flex flex-row gap-y-2">
-									<GraduationCapIcon className='w-10 text-pink-400' />
-									<h2 className="text-lg font-medium">Collage</h2>
-								</div>
-								<HeartIcon className='w-10 hover:text-pink-400' />
-							</div>
-						</div>
-					</div>
-				</Link>
+			<div className="my-4">
+				<Swiper
+					slidesPerView={isMobile ? 1 : 4}
+					spaceBetween={5}
+					autoplay={{
+						delay: 4000,
+						disableOnInteraction: false,
+					}}
+					pagination={{
+						clickable: true,
+					}}
+					navigation={true}
+					modules={[Autoplay, Navigation]}
+					className="mySwiper"
+				>
+					<SwiperSlide>
+						<Image src="/london.jpg" className=' w-full object-cover' alt='banner' width={1280} height={400} />
+					</SwiperSlide>
+					<SwiperSlide>
+						<Image src="/london.jpg" className=' w-full object-cover' alt='banner' width={1280} height={400} />
+					</SwiperSlide>
+					<SwiperSlide>
+						<Image src="/london.jpg" className=' w-full object-cover' alt='banner' width={1280} height={400} />
+					</SwiperSlide>
+					<SwiperSlide>
+						<Image src="/london.jpg" className=' w-full object-cover' alt='banner' width={1280} height={400} />
+					</SwiperSlide>
+				</Swiper>
 			</div>
 		</div>
 	)

3f8e33fe404522bf18953edd7ae711fa4914f2ca
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-12 08:19:49 +0600

    [ADD] sever start

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
+
 function Education() {
 	return (
 		<div className='mx-2 md:mx-4 my-4'>
@@ -10,7 +11,7 @@ function Education() {
 				<h2 className="px-4 py-2 rounded md:text-xl text-sm font-medium text-white bg-pink-400 w-fit">Educational Institutions in Kurigram</h2>
 			</div>
 			<div className="mt-4 grid md:grid-cols-4  grid-cols-1 gap-3">
-				<Link href="/">
+				<Link href="/education">
 					<div className="flex justify-center flex-col mt-4 rounded-md border-[1px] hover:border-pink-400 hover:border-2 shadow-lg">
 						<Image src="/room.jpg" className=' rounded-md object-cover w-full' alt='room' width={300} height={200} priority={true} />
 						<div className="px-2 py-4 flex flex-col gap-x-3">

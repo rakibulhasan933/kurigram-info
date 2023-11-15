a5ac7a3da390ae3b8c4630c65778d54327c59870
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-15 22:36:50 +0600

    [ADD] fix

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-	return (
-		<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50"> <SignIn /></div>
-	);
-}

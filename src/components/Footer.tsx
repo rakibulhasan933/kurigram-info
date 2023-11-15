32736d3140d9d8e29f20c223b61279274767087a
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-16 02:58:19 +0600

    [ADD] footer added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,101 +5,56 @@ import { isBrowser, isMobile } from 'react-device-detect'
 
 function Footer() {
 	return (
-		<div className='mx-2 md:mx-4'>
-			<div className=" bg-slate-100">
-				<div className='grid md:grid-cols-4  py-4 px-2 gap-5 rounded'>
-					<div className="">
+		<div className='bg-slate-100 md:px-10 px-2 py-4'>
+			<div>
+				<div className='flex md:flex-row flex-col justify-between items-center py-4 px-2 md:gap-6 gap-3 rounded'>
+					<div className="md:basis-1/2 w-full">
 						<iframe className='w-full rounded' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d919515.5403311296!2d89.00917749514751!3d25.80827225152525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e2c8648556f8cb%3A0xfaa7d471019abb1f!2sKurigram%20District!5e0!3m2!1sen!2sbd!4v1699635212697!5m2!1sen!2sbd" width="500" height="300" style={{ border: 0 }} loading="lazy"></iframe>
 					</div>
+					<div className="md:basis-1/2 w-full flex flex-row items-center justify-between md:px-8 px-4">
+						<div className="flex flex-col md:px-4 px-2">
+							<h1 className='text-xl mt-8 font-bold font-sans opacity-75 underline'>Social Media</h1>
+							<ol className='space-y-2 text-sm mt-5'>
+								<li className='flex gap-2'>
+									<FacebookIcon className=' text-xl  text-blue-600 font-bold' />
+									<Link className='font-semibold hover:underline' href="https://www.facebook.com/profile.php?id=100093723266018" >
+										Facebook
+									</Link>
+								</li>
+								<li className='flex gap-2'>
+									<YoutubeIcon className=' text-xl  text-red-600 font-bold' />
+									<Link className='font-semibold hover:underline' href="" >
+										Youtube
+									</Link>
+								</li>
 
-					{isBrowser && (
-						<>
-							<div className="flex flex-col md:px-4 px-2">
-								<h1 className='text-xl mt-8 font-bold font-sans opacity-75 underline  '>Our Services</h1>
-								<ol className='space-y-2 text-sm mt-5'>
-									<li>
-										<Link href="/hotels" className='font-medium'>Hotels</Link>
-									</li>
-									<li>
-										<Link href="/restaurants" className='font-medium'>Restaurants</Link>
-									</li>
-									<li>
-										<Link href="/places" className='font-medium'>Places</Link>
-									</li>
-									<li>
-										<Link href="/education" className='font-medium'>Institutions</Link>
-									</li>
-								</ol>
-							</div>
-							<div className="flex flex-col md:px-4 px-2">
-								<h1 className='text-xl mt-8 font-bold font-sans opacity-75 underline  '>Social Media</h1>
-								<ol className='space-y-2 text-sm mt-5'>
-									<li className='flex gap-2'>
-										<FacebookIcon className=' text-xl  text-blue-600 font-bold' />
-										<Link className='font-semibold hover:underline' href="https://www.facebook.com/profile.php?id=100093723266018" >
-											Facebook
-										</Link>
-									</li>
-									<li className='flex gap-2'>
-										<YoutubeIcon className=' text-xl  text-red-600 font-bold' />
-										<Link className='font-semibold hover:underline' href="" >
-											Youtube
-										</Link>
-									</li>
-									<li className='flex gap-2'>
-										<LinkedinIcon className=' text-xl  text-blue-600 font-bold' />
-										<Link className='font-semibold hover:underline' href=" " >
-											Linkedin
-										</Link>
-									</li>
-
-									<li className='flex gap-2'>
-										<PhoneCallIcon className=' text-xl  text-green-500 font-bold' />
-										<Link className='font-semibold hover:underline' href="" >
-											+8801318180268
-										</Link>
-									</li>
-								</ol>
-							</div>
-							<div className="md:px-4 px-2">
-								<h1 className='text-xl mt-8 font-bold font-sans opacity-75 underline  '>Contact Us</h1>
-								<ol className='space-y-2 text-sm mt-5'>
-									<li className='flex gap-2'>
-										<PhoneIncomingIcon />
-										<Link href="" className='font-medium'> +8801318180268</Link>
-									</li>
-									<li className='flex gap-2'>
-										<PhoneForwardedIcon />
-										<Link href="" className='font-medium'> +8801890570355</Link>
-									</li>
-									<li className='flex gap-2'>
-										<MailCheckIcon />
-										<Link href="" className='font-medium'>kurigram@info.com</Link>
-									</li>
-
-								</ol>
-							</div>
-						</>
-					)}
-					{
-						isMobile && (
-
-							<div className='space-y-2 flex flex-row text-sm mt-5'>
-								<h3 className='flex gap-2'>
+								<li className='flex gap-2'>
+									<PhoneCallIcon className=' text-xl  text-green-500 font-bold' />
+									<Link className='font-semibold hover:underline' href="" >
+										+8801318180268
+									</Link>
+								</li>
+							</ol>
+						</div>
+						<div className="md:px-4 px-2">
+							<h1 className='text-xl mt-8 font-bold font-sans opacity-75 underline  '>Contact Us</h1>
+							<ol className='space-y-2 text-sm mt-5'>
+								<li className='flex gap-2'>
 									<PhoneIncomingIcon />
-
-								</h3>
-								<h3 className='flex gap-2'>
+									<Link href="" className='font-medium'> +8801318180268</Link>
+								</li>
+								<li className='flex gap-2'>
 									<PhoneForwardedIcon />
-
-								</h3>
-								<h3 className='flex gap-2'>
+									<Link href="" className='font-medium'> +8801890570355</Link>
+								</li>
+								<li className='flex gap-2'>
 									<MailCheckIcon />
-								</h3>
-							</div>
-						)
-					}
+									<Link href="" className='font-medium'>kurigram@info.com</Link>
+								</li>
 
+							</ol>
+						</div>
+					</div>
 				</div>
 				<h1 className='text-center opacity-70 text-sm p-5 '>©Kurigram.Info.™ is a registered trademark. All Rights Reserved.Development by <a href="http://rakibul-hasan-dev.web.app" target="_blank" rel="noopener noreferrer">Rakibul hasan</a></h1>
 			</div>

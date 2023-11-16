32f1a8d1a66c8d5a39dbba657d1b263a4beda224
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-16 13:05:29 +0600

    [ADD] team

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -17,7 +17,7 @@ async function Stats() {
 	return (
 		<div className="bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900 py-24 sm:py-32">
 			<div className="mx-auto max-w-7xl px-6 lg:px-8">
-				<dl className="grid  gap-x-4 gap-y-8 text-center grid-cols-4">
+				<dl className="grid  gap-x-2 gap-y-4 text-center grid-cols-4">
 					{stats.map((stat) => (
 						<div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
 							<dt className="text-base text-gray-300 leading-7">{stat.name}</dt>

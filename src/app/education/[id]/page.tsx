0d40afa2350fd62999086a7a80964d2b62fa9537
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-16 15:34:12 +0600

    [ADD] education

--- src/app/education/[id]/page.tsx
+++ src/app/education/[id]/page.tsx
@@ -23,13 +23,8 @@ async function EducationCard({ params }: ParamsIProps) {
 		<div className='bg-gray-50'>
 			<SliderCard data={data} />
 			<div className="mx-2 md:mx-4 my-8">
-				<div className="flex justify-between md:flex-row flex-col gap-4  px-2  md:px-12 py-2">
-					<div className="flex flex-col">
-						<h1 className="text-xl font-bold mb-2">{data?.title}</h1>
-					</div>
-					<div className="">
-						<Button className=' border-2 hover:border-pink-400 hover:text-pink-400 md:mr-4 mr-2' variant="secondary"><HeartIcon className='w-10 text-pink-400 mr-2' />Favorite</Button>
-					</div>
+				<div className="flex flex-col gap-4  px-2  md:px-12 py-2">
+					<h1 className="md:text-2xl text-xl  font-bold mb-2">{data?.title}</h1>
 				</div>
 				<hr className='w-full h-2 my-4' />
 				<div className="px-2 md:px-4">
@@ -42,28 +37,20 @@ async function EducationCard({ params }: ParamsIProps) {
 									<p className=" text-sm font-normal">{data?.description}</p>
 								</div>
 							</div>
-							<div className="bg-white px-3 py-4">
-								<Review id={id} />
-							</div>
+							<TeacherCard />
 						</div>
 						<div className="flex flex-col gap-y-4 gap-2 mb-4">
 							<div className="bg-white px-3 py-4 rounded">
 								<h2 className="uppercase p-1 flex flex-row font-bold"><Bookmark className='w-4 mr-1' />Contacts INFO</h2>
 								<hr className='w-full' />
 								<div className="px-3 py-4  flex flex-col gap-y-5">
-									<h1 className=" text-sm flex flex-row items-center font-medium hover:text-pink-400"> <MailIcon className='w-6 mr-3' /> info@perapalace.com</h1>
-									<h1 className=" text-sm flex flex-row items-center font-medium  hover:text-pink-400"> <PhoneCallIcon className='w-6 mr-3' />+8801793874052 </h1>
-									<h1 className=" text-sm flex flex-row items-center font-medium  hover:text-pink-400"> <Globe className='w-6 mr-3' />https://www.perapalace.com</h1>
-									<h1 className=" text-sm flex flex-row items-center font-medium  hover:text-pink-400"> <MapPin className='w-6 mr-3' />Kurigram</h1>
+									<h1 className=" text-sm flex flex-row items-center font-medium hover:text-pink-400"> <MailIcon className='w-6 mr-3' color='blue' /> info@perapalace.com</h1>
+									<h1 className=" text-sm flex flex-row items-center font-medium  hover:text-pink-400"> <Globe className='w-6 mr-3' color='blue' />https://www.perapalace.com</h1>
+									<h1 className=" text-sm flex flex-row items-center font-medium  hover:text-pink-400"> <MapPin className='w-6 mr-3' color='blue' />Kurigram</h1>
 								</div>
 							</div>
-							<TeacherCard />
-							<div className="bg-white rounded">
-								<h2 className="p-1 flex flex-row font-bold"><Bookmark className='w-4 mr-1' />Map</h2>
-								<hr className='w-full my-3' />
-								<div className="">
-									<iframe className='w-full rounded' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d919515.5403311296!2d89.00917749514751!3d25.80827225152525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e2c8648556f8cb%3A0xfaa7d471019abb1f!2sKurigram%20District!5e0!3m2!1sen!2sbd!4v1699635212697!5m2!1sen!2sbd" width="500" height="300" style={{ border: 0 }} loading="lazy"></iframe>
-								</div>
+							<div className="bg-white px-3 py-4">
+								<Review id={id} />
 							</div>
 						</div>
 					</div>
--- src/components/TeacherCard.tsx
+++ src/components/TeacherCard.tsx
@@ -3,22 +3,35 @@ import React from 'react'
 
 function TeacherCard() {
 	return (
-		<>
-			<div className="flex flex-row gap-x-1 items-center rounded-md cursor-pointer bg-white hover:border-pink-400 hover:border-[2px] shadow-xl">
-				<Image src="/pizza.jpg" className=' object-cover rounded' alt='pizza' width={150} height={120} />
-				<div className="p-2">
-					<h2 className=" text-lg font-bold">MD Rakibul Hasan</h2>
-					<p className="text-sm font-normal">Kurigram restaurant</p>
+		<div className="">
+			<h2 className=' text-xl font-semibold py-4 text-center'>Teacher List</h2>
+			<div className='flex flex-row items-stretch gap-3'>
+				<div className="basis-1/3 flex flex-col gap-x-1 items-center rounded-md cursor-pointer bg-white hover:border-pink-400 hover:border-[1px] shadow-xl p-3">
+					<Image src="/teacher.jpg" className=' object-cover rounded' alt='pizza' width={100} height={100} />
+					<div className="p-2">
+						<h2 className="text-base mb-1 font-bold">MD Rakibul Hasan</h2>
+						<p className="text-sm  mb-1 font-medium">English Department</p>
+						<p className="text-sm mb-1 font-bold text-blue-400">+8801700000000</p>
+					</div>
+				</div>
+				<div className="basis-1/3 flex flex-col gap-x-1 items-center rounded-md cursor-pointer bg-white hover:border-pink-400 hover:border-[1px] shadow-xl p-3">
+					<Image src="/teacher.jpg" className=' object-cover rounded' alt='pizza' width={100} height={100} />
+					<div className="p-2">
+						<h2 className="text-base mb-1 font-bold">MD Rakibul Hasan</h2>
+						<p className="text-sm  mb-1 font-medium">English Department</p>
+						<p className="text-sm mb-1 font-bold text-blue-400">+8801700000000</p>
+					</div>
+				</div>
+				<div className="basis-1/3 flex flex-col gap-x-1 items-center rounded-md cursor-pointer bg-white hover:border-pink-400 hover:border-[1px] shadow-xl p-3">
+					<Image src="/teacher.jpg" className=' object-cover rounded' alt='pizza' width={100} height={100} />
+					<div className="p-2">
+						<h2 className="text-base mb-1 font-bold">MD Rakibul Hasan</h2>
+						<p className="text-sm  mb-1 font-medium">English Department</p>
+						<p className="text-sm mb-1 font-bold text-blue-400">+8801700000000</p>
+					</div>
 				</div>
 			</div>
-			<div className="flex flex-row items-center gap-x-1 rounded-md cursor-pointer bg-white hover:border-pink-400 hover:border-[2px] shadow-xl">
-				<Image src="/pizza.jpg" className=' object-cover rounded' alt='pizza' width={150} height={120} />
-				<div className="p-2">
-					<h2 className=" text-lg font-bold">MD Rakibul Hasan</h2>
-					<p className="text-sm font-normal">Kurigram restaurant</p>
-				</div>
-			</div>
-		</>
+		</div>
 	)
 }
 

45a0a232a07a89241604e022805191fb56253ebe
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-16 13:18:30 +0600

    [ADD] helpline

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -16,8 +16,8 @@ async function Stats() {
 	]
 	return (
 		<div className="bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900 py-24 sm:py-32">
-			<div className="mx-auto max-w-7xl px-6 lg:px-8">
-				<dl className="grid  gap-x-2 gap-y-4 text-center grid-cols-4">
+			<div className="mx-auto max-w-7xl px-2 lg:px-8">
+				<dl className="grid  gap-4 text-center grid-cols-4">
 					{stats.map((stat) => (
 						<div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
 							<dt className="text-base text-gray-300 leading-7">{stat.name}</dt>

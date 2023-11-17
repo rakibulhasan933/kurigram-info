be3137bda7504707cd8a0a7167ed3f9669ff2bf2
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-17 19:37:15 +0600

    [ADD] maps added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,11 +10,11 @@ import { isMobile } from 'react-device-detect';
 function Navbar() {
 
 	return (
-		<div className='py-3 md:px-4 bg-slate-200'>
+		<div className='py-3 md:px-4 bg-blue-300'>
 			<div className="flex flex-row justify-between items-center">
-				<Link className='font-bold md:text-lg text-sm hover:text-blue-400 ' href="/">Kurigram</Link>
+				<Link className='font-bold md:text-lg text-sm' href="/">Kurigram</Link>
 				{
-					isMobile ? (<Link href="/search" className="hover:text-blue-400 text-sm flex flex-row items-center font-semibold"><SearchIcon className='w-5 ml-2' /></Link>) : (<Link href="/search" className=" hover:text-blue-400 text-sm flex flex-row items-center font-semibold">What are you Looking for <SearchIcon className='w-5 ml-2' /></Link>)
+					isMobile ? (<Link href="/search" className="hover:text-blue-400 text-sm flex flex-row items-center font-semibold"><SearchIcon className='w-5 ml-2' /></Link>) : (<Link href="/search" className=" hover:text--400 text-sm flex flex-row items-center font-semibold">What are you Looking for <SearchIcon className='w-5 ml-2' /></Link>)
 				}
 				<DownMenu />
 			</div>
